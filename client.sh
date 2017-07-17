883257899de0239d1fea1a14ba607f5994e5bfd9
Leonard Huang <leihuang.org@gmail.com>
2017-07-17 12:45:28 +0800

    Update client.sh

--- client.sh
+++ client.sh
@@ -9,10 +9,10 @@ sed -i "s/password=my_password/password=$password/" "/etc/shadowvpn/client.conf"
 read -p "shadowvpn intf: " intf
 sed -i "s/intf=tun0/intf=$intf/" "/etc/shadowvpn/client.conf"
 
-sed -i "s/ip route add   0/1 dev $intf//" "/etc/shadowvpn/client_up.sh"
-sed -i "s/ip route add 128/1 dev $intf/echo 'namesever 8.8.8.8' > /etc/reslov.conf/" "/etc/shadowvpn/client_up.sh"
+sed -i "s/ip route add   0\/1 dev \$intf//" "/etc/shadowvpn/client_up.sh"
+sed -i "s/ip route add 128\/1 dev \$intf/echo 'namesever 8.8.8.8' > \/etc\/reslov.conf/" "/etc/shadowvpn/client_up.sh"
 
-sed -i "s/ip route del   0/1//" "/etc/shadowvpn/client_down.sh"
-sed -i "s/ip route del 128/1/echo 'namesever 114.114.114.114' > /etc/reslov.conf" "/etc/shadowvpn/client_down.sh"
+sed -i "s/ip route del   0\/1//" "/etc/shadowvpn/client_down.sh"
+sed -i "s/ip route del 128\/1/echo 'namesever 114.114.114.114' > \/etc\/reslov.conf/" "/etc/shadowvpn/client_down.sh"
 
 /etc/init.d/shadowvpn restart
