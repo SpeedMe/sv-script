60432f5ac1b5cc2ff1b6adca18381bbadfc274a9
Leonard Huang <leihuang.org@gmail.com>
2017-07-17 12:26:36 +0800

    Create client.sh

---
+++ client.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+echo "deb http://shadowvpn.org/debian wheezy main">>/etc/apt/sources.list
+apt-get update
+apt-get install -y --force-yes  shadowvpn
+sed -i "s/CONFFILE="/etc/shadowvpn/server.conf"/CONFFILE="/etc/shadowvpn/client.conf"/" "/etc/default/shadowvpn"
+sudo update-rc.d shadowvpn disable
+read -p "shadowvpn password: " password
+sed -i "s/password=my_password/password=$password/" "/etc/shadowvpn/client.conf"
+read -p "shadowvpn intf: " intf
+sed -i "s/intf=tun0/intf=$intf/" "/etc/shadowvpn/client.conf"
+
+sed -i "s/ip route add   0/1 dev $intf//" "/etc/shadowvpn/client_up.sh"
+sed -i "s/ip route add 128/1 dev $intf/echo 'namesever 8.8.8.8' > /etc/reslov.conf/" "/etc/shadowvpn/client_up.sh"
+
+sed -i "s/ip route del   0/1//" "/etc/shadowvpn/client_down.sh"
+sed -i "s/ip route del 128/1/echo 'namesever 114.114.114.114' > /etc/reslov.conf" "/etc/shadowvpn/client_down.sh"
+
+/etc/init.d/shadowvpn restart
